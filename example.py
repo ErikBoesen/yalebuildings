582bf50798915da864da9e9050a75a1f191a1d82
ErikBoesen <me@erikboesen.com>
2019-06-20 17:59:43 -0400

    Start writing actual example

--- example.py
+++ example.py
@@ -3,3 +3,6 @@ import os
 
 # "api" name can be whatever is most convenient for your program
 api = yalebuildings.YaleBuildings(os.environ['YALE_API_TOKEN'])
+
+for building in api.buildings():
+    print(building['BUILDING'])
