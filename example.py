1c79f8f3604020d2b51b2b9372236c65e5b8ea0e
ErikBoesen <me@erikboesen.com>
2019-06-21 23:09:37 -0400

    Update api key environment variable

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import yalebuildings
 import os
 
 # "api" name can be whatever is most convenient for your program
-api = yalebuildings.YaleBuildings(os.environ['YALE_API_TOKEN'])
+api = yalebuildings.YaleBuildings(os.environ['YALE_API_KEY'])
 
 for building in api.buildings():
     print(f"{building.id}: {building.name}")
